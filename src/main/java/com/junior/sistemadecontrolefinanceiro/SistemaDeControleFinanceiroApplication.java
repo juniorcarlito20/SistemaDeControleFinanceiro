12eaff244f70ee6a27b32b2b1e74d492f7a5333f
Junior Rodrigues <juniorcarlito20@hotmail.com>
2026-05-19 20:45:26 -0300

    feat: estrutura inicial do projeto

---
+++ src/main/java/com/junior/sistemadecontrolefinanceiro/SistemaDeControleFinanceiroApplication.java
@@ -0,0 +1,13 @@
+package com.junior.sistemadecontrolefinanceiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeControleFinanceiroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaDeControleFinanceiroApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/junior/sistemadecontrolefinanceiro/SistemaDeControleFinanceiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junior.sistemadecontrolefinanceiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeControleFinanceiroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
