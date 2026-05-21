3649d71d1fc55a680eda1c9d4204bac4dca63426
Junior Rodrigues <juniorcarlito20@hotmail.com>
2026-05-21 20:49:04 -0300

    feat: CRUD completo da entidade User

---
+++ src/main/java/com/junior/sistemadecontrolefinanceiro/controller/UserController.java
@@ -0,0 +1,43 @@
+package com.junior.sistemadecontrolefinanceiro.controller;
+
+import com.junior.sistemadecontrolefinanceiro.entity.User;
+import com.junior.sistemadecontrolefinanceiro.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long id) {
+        return userService.getUserById(id);
+    }
+    @PutMapping("/{id}")
+    public User updateUser(@PathVariable Long id,
+                           @RequestBody User updatedUser) {
+
+        return userService.updateUser(id, updatedUser);
+    }
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+    }
+
+}
--- src/main/java/com/junior/sistemadecontrolefinanceiro/service/UserService.java
+++ src/main/java/com/junior/sistemadecontrolefinanceiro/service/UserService.java
@@ -1,12 +1,48 @@
 package com.junior.sistemadecontrolefinanceiro.service;
 
+import com.junior.sistemadecontrolefinanceiro.entity.User;
 import com.junior.sistemadecontrolefinanceiro.repository.UserRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserService {
     private final UserRepository userRepository;
+
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
+
+    //Metodo para criar os usuarios
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    // metodo para listar todos os usuarios
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    //Metodo para buscar usuario por Id
+    public User getUserById(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+     //Metodo para atualizar usuario
+    public User updateUser(Long id, User userDetails) {
+        User user = userRepository.findById(id).orElse(null);
+        if (user != null) {
+            user.setName(userDetails.getName());
+            user.setEmail(userDetails.getEmail());
+            user.setPassword(userDetails.getPassword());
+            return userRepository.save(user);
+        }
+        return null;
+    }
+
+     //Metodo para deletar usuario
+     public void deleteUser(Long id) {
+         userRepository.deleteById(id);
+     }
 }
