6a7838d93e01f17d3b7ddd672c8102008e681911
Junior Rodrigues <juniorcarlito20@hotmail.com>
2026-05-31 14:10:47 -0300

    feat: implementado CRUD completo de contas e relacionamento com usuarios

---
+++ src/main/java/com/junior/sistemadecontrolefinanceiro/controller/AccountController.java
@@ -0,0 +1,52 @@
+package com.junior.sistemadecontrolefinanceiro.controller;
+
+import com.junior.sistemadecontrolefinanceiro.dto.AccountRequestDTO;
+import com.junior.sistemadecontrolefinanceiro.dto.AccountResponseDTO;
+import com.junior.sistemadecontrolefinanceiro.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/accounts")
+public class AccountController {
+
+    private final AccountService accountService;
+
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @PostMapping
+    public ResponseEntity<AccountResponseDTO> create(
+            @RequestBody AccountRequestDTO dto) {
+        return ResponseEntity.ok(accountService.createAccount(dto));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AccountResponseDTO> getById(@PathVariable Long id) {
+        return ResponseEntity.ok(accountService.getAccountById(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<AccountResponseDTO>> getAllAccounts() {
+        return ResponseEntity.ok(accountService.getAllAccounts());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<AccountResponseDTO> updateAccount(
+            @PathVariable Long id,
+            @RequestBody AccountRequestDTO dto) {
+
+        return ResponseEntity.ok(accountService.updateAccount(id, dto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteAccount(@PathVariable Long id) {
+        accountService.deleteAccount(id);
+        return ResponseEntity.noContent().build();
+    }
+}
