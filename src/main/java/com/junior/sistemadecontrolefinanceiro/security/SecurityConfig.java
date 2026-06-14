24c5f7a8b30a9c88b9d422929fb7c061221ed699
Junior Rodrigues <juniorcarlito20@hotmail.com>
2026-06-13 21:44:39 -0300

    cofiguracao do CORS

--- src/main/java/com/junior/sistemadecontrolefinanceiro/security/SecurityConfig.java
+++ src/main/java/com/junior/sistemadecontrolefinanceiro/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -28,6 +29,8 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable()) // Desabilitado pois usamos JWT (Stateless)
+
+                .cors(Customizer.withDefaults()) // Habilita CORS com configuração padrão (pode ser personalizada se necessário)
 
                 // Define a política de sessão como STATELESS (não cria cookies de sessão)
                 .sessionManagement(session -> session
