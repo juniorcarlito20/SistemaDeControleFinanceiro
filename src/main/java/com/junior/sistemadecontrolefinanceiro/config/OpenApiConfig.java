ee5752dc47f7ce0995dbee651920ef9f9c4b1ae0
Junior Rodrigues <juniorcarlito20@hotmail.com>
2026-06-13 22:05:43 -0300

    corrigido swagger server url produção railway

--- src/main/java/com/junior/sistemadecontrolefinanceiro/config/OpenApiConfig.java
+++ src/main/java/com/junior/sistemadecontrolefinanceiro/config/OpenApiConfig.java
@@ -1,25 +1,23 @@
 package com.junior.sistemadecontrolefinanceiro.config;
 
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
 @Configuration
-@SecurityScheme(
-        name = "bearerAuth",
-        type = SecuritySchemeType.HTTP,
-        scheme = "bearer",
-        bearerFormat = "JWT"
-)
 public class OpenApiConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
+                .servers(List.of(
+                        new Server().url("https://sistemadecontrolefinanceiro.up.railway.app")
+                ))
                 .info(new Info()
                         .title("Sistema de Controle Financeiro API")
                         .version("1.0")
